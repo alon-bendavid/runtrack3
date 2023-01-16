fdbea03c4327f7afcaceb48db13066a54c5c23b4
alon-bendavid <ben-david.alon@laplateforme.io>
2023-01-16 19:23:02 +0100

    job05

--- jour01/job05/script.js
+++ jour01/job05/script.js
@@ -1,4 +1,14 @@
- function afficherjourssemaines(){
 
-
- }
+function getWeekDates(){
+    const dates = [new Date()]; // today
+    const curr = new Date();
+    const daysCount = 7-curr.getDay();
+    for(let i=1; i<= daysCount; i++){
+      // increase current Date by 1 and set to current Date
+        const nextDate = curr.setDate(curr.getDate()+1);
+      dates.push(new Date(nextDate));
+    }
+    return dates;
+  }
+  
+  console.log(getWeekDates());
