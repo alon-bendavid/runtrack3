e914568ce2dd14b82a323f21ca82307ef55d641a
alon-bendavid <ben-david.alon@laplateforme.io>
2023-01-16 12:17:58 +0100

    job04

---
+++ jour01/job04/script.js
@@ -0,0 +1,14 @@
+ const année = 6;
+ function bisextile(année){
+     if ( année == "bisextile"){
+
+         return true;
+
+     }
+     else{
+        return false;
+
+     }
+
+ }
+ console.log(bisextile("bisextile"));
