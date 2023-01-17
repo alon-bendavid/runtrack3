0db2756534debb82368f010ac36d2a4a3b9bae52
alon-bendavid <ben-david.alon@laplateforme.io>
2023-01-17 11:28:03 +0100

    job01

---
+++ jour02/job01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Citation Example</title>
+</head>
+
+<body>
+    <article id="citation">La vie a beaucoup plus d’imagination que nous</article>
+    <button id="button">Click me</button>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,9 @@
+
+
+
+document.getElementById("button").addEventListener("click",citation);
+function citation(){
+
+  var citationContent = document.getElementById("citation").innerHTML;
+  console.log(citationContent);
+}
