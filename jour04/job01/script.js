2ea3e477d3c67459a214bc484f13770fa64b9dcb
alon-bendavid <ben-david.alon@laplateforme.io>
2023-01-19 14:18:41 +0100

    job01

---
+++ jour04/job01/script.js
@@ -0,0 +1,11 @@
+var button = document.getElementById('button');
+
+button.addEventListener('click', function() {
+  fetch('expression.txt')
+    .then(response => response.text())
+    .then(data => {
+      var p = document.createElement('p');
+      p.innerText = data;
+      document.body.appendChild(p);
+    });
+});
