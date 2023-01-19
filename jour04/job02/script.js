b9223699a83d9fa401689a12a61a250a1d9e76ed
alon-bendavid <ben-david.alon@laplateforme.io>
2023-01-19 19:41:37 +0100

    job02

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,12 +1,5 @@
 
-function getValueByKey(data, key) {
-  if(data.hasOwnProperty(key)){
-      return data[key];
-  }else{
-      return 'Key not found';
-  }
-}
-var data = {
+var object= {
   name: "La Plateforme_",
   address: "8 rue d'hozier",
   city: "Marseille",
@@ -14,5 +7,34 @@ var data = {
   creation:"2019"
 };
 
-var city = getValueByKey(data, "city");
-console.log(city); 
+function getValueByKey(data, key) {
+  if(data.hasOwnProperty(key)){
+      return data[key];
+  }else{
+      return 'Key not found';
+  }
+}
+console.log(getValueByKey(object, 'city'));
+
+
+
+
+
+
+// function getValueByKey(data, key) {
+//   if(data.hasOwnProperty(key)){
+//       return data[key];
+//   }else{
+//       return 'Key not found';
+//   }
+// }
+// var data = {
+//   name: "La Plateforme_",
+//   address: "8 rue d'hozier",
+//   city: "Marseille",
+//   nb_staff: "11",
+//   creation:"2019"
+// };
+
+// var city = getValueByKey(data, "city");
+// console.log(city); 
